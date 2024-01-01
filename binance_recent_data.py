cb2740bb46cbf6718f3b27099030a463d3d02506
beggardie <rubismoreira@gmail.com>
2024-01-01 19:50:43 +0100

    Add db features

--- binance_recent_data.py
+++ binance_recent_data.py
@@ -1,14 +1,32 @@
-from binance import Client
 import pandas as pd
-def get_recent_data(filename_output, api_key, api_secret, symbol, data_type,range_start_cols,range_end_cols):
-    try:
-        file = open("kline_data.txt", "x")
-    except:
-        file = open("kline_data.txt", "w")
-    bin_client = Client(api_key, api_secret)
-    df_recent = pd.DataFrame(bin_client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1MINUTE, limit=1000))
-    df_recent = df_recent.iloc[:, range_start_cols:range_end_cols]
+from binance import Client
+import db_driver
 
-    with open("kline_data.txt", "a") as file:
-        file.write(df_recent.to_string(header=False, index=False))
-    return df_recent
+def handle_binance_recent_data(filename_output, api_key, api_secret, symbol, range_start_cols, range_end_cols, db_conn,data_type):
+    #queries
+    data_return = query_binance(api_key, api_secret, symbol)
+    #write_to_file
+    write_data_to_file(filename_output, data_return, range_start_cols, range_end_cols)
+    #write_on_db
+    #todo add for all tables
+    if data_type == 'klines':
+        db_driver.insert_data_from_dataframe(data_return, db_conn, 'kline_data')
+
+    return data_return;
+
+def query_binance(api_key, api_secret, symbol):
+    bin_client = Client(api_key, api_secret)
+    resulting_df = pd.DataFrame(bin_client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1MINUTE, limit=1000))
+    return resulting_df
+
+def write_data_to_file(filename_output, queried_df, range_start_cols, range_end_cols):
+    try:
+        file = open(filename_output, "x")
+    except:
+        file = open(filename_output, "w")
+    df_recent_write = queried_df.iloc[:, range_start_cols:range_end_cols]
+
+    with open(filename_output, "a") as file:
+        file.write(df_recent_write.to_string(header=False, index=False))
+    return df_recent_write
+
--- binance_streams.py
+++ binance_streams.py
@@ -1,23 +1,27 @@
-from binance import AsyncClient, BinanceSocketManager, Client
 import asyncio
+
 import pandas as pd
+from binance import AsyncClient, BinanceSocketManager, Client
 
 # Define which keys to keep from streams
 keys_candles = ["t", "o", "h", "l", "c", "V"]
 key_agg_trades = ["a", "p", "q", "f", "l", "T"]
-colnames_agg_trades = ["AggTradeID","Price","Quantity","FirstTradeID","LastTradeID","Timestamp"]
+colnames_agg_trades = ["AggTradeID", "Price", "Quantity", "FirstTradeID", "LastTradeID", "Timestamp"]
+
+
 def ask_exit(signame, loop):
     print("got signal %s: exit" % signame)
     loop.stop()
 
-async def get_kline_data(bsm,api_key, api_secret, symbol):
+
+async def get_kline_data(bsm, api_key, api_secret, symbol):
     try:
         file = open("kline_data.txt", "x")
     except:
         file = open("kline_data.txt", "w")
     bin_client = Client(api_key, api_secret)
     df_recent = pd.DataFrame(bin_client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1MINUTE, limit=1000))
-    df_recent = df_recent.iloc[:,0:6]
+    df_recent = df_recent.iloc[:, 0:6]
 
     with open("kline_data.txt", "a") as file:
         file.write(df_recent.to_string(header=False, index=False))
@@ -30,20 +34,19 @@ async def get_kline_data(bsm,api_key, api_secret, symbol):
             res_df = pd.DataFrame(res_dict_selected_keys, index=pd.Series(res_dict["s"]))
             with open('kline_data.txt', 'a') as file:
                 file.write(f'\n')
-                file.write(res_df.to_string(header=False,index=False))
+                file.write(res_df.to_string(header=False, index=False))
                 file.close()
             print(res)
 
 
-
-async def get_aggr_trade_data(bsm,api_key, api_secret, symbol):
+async def get_aggr_trade_data(bsm, api_key, api_secret, symbol):
     try:
         file = open("aggr_trades.txt", "x")
     except:
         file = open("aggr_trades.txt", "w")
     bin_client = Client(api_key, api_secret)
     df_recent = pd.DataFrame(bin_client.get_aggregate_trades(symbol=symbol, limit=1000))
-    df_recent = df_recent.iloc[:,0:6]
+    df_recent = df_recent.iloc[:, 0:6]
 
     with open("aggr_trades.txt", "a") as file:
         file.write(df_recent.to_string(header=False, index=False))
@@ -57,22 +60,24 @@ async def get_aggr_trade_data(bsm,api_key, api_secret, symbol):
             res_df = pd.DataFrame(res_dict_selected_keys, index=pd.Series(res_dict["s"]))
             with open('aggr_trades.txt', 'a') as file:
                 file.write(f'\n')
-                file.write(res_df.to_string(header=False,index=False))
+                file.write(res_df.to_string(header=False, index=False))
                 file.close()
             print(res)
 
-async def main(api_key, api_secret, coin,fiat_curr, flag_use_demo_acc):
+
+async def main(api_key, api_secret, coin, fiat_curr, flag_use_demo_acc):
     client = await AsyncClient.create(api_key, api_secret, testnet=flag_use_demo_acc)
     bsm = BinanceSocketManager(client, user_timeout=20)
 
     symbol_txt = coin + fiat_curr
-    await asyncio.gather(get_kline_data(bsm,api_key, api_secret, symbol_txt),
-                         get_aggr_trade_data(bsm,api_key, api_secret, symbol_txt))
+    await asyncio.gather(get_kline_data(bsm, api_key, api_secret, symbol_txt),
+                         get_aggr_trade_data(bsm, api_key, api_secret, symbol_txt))
     await asyncio.sleep(10)
     await client.close_connection()
 
-def run_main(api_key, api_secret, coin="ETH", fiat_curr="USDT",flag_use_demo_acc=True):
+
+def run_main(api_key, api_secret, coin="ETH", fiat_curr="USDT", flag_use_demo_acc=True):
     return asyncio.run(main(api_key, api_secret, coin, fiat_curr, flag_use_demo_acc))
 
-#if __name__ == "__main__":
-#asyncio.run(main())
+# if __name__ == "__main__":
+# asyncio.run(main())
